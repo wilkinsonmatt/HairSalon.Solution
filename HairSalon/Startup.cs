3d156f9bb7adc6b1da1933f5a3458cec7cbf386c
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-08-02 14:52:01 -0700

    something broke

--- HairSalon/Startup.cs
+++ HairSalon/Startup.cs
@@ -43,7 +43,7 @@ namespace HairSalon
 
       app.Run(async (context) =>
       {
-        await context.Response.WriteAsync("Hello World!");
+        await context.Response.WriteAsync("Something broke");
       });
     }
   }
