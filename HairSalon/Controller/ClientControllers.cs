30a98c3063451880540d3fb20c1c70449c0f4bcd
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-08-02 14:51:02 -0700

    fixed formatting

--- HairSalon/Controller/ClientControllers.cs
+++ HairSalon/Controller/ClientControllers.cs
@@ -30,11 +30,27 @@ namespace HairSalon.Controllers
       return View();
     }
 
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+        _db.Clients.Add(client);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+
     public ActionResult Edit(int id)
     {
       Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
       ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
       return View(thisClient);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Client client)
+    {
+      _db.Entry(client).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
 
     public ActionResult Delete(int id)
@@ -52,26 +68,10 @@ namespace HairSalon.Controllers
         return RedirectToAction("Index");
     }
 
-    [HttpPost]
-    public ActionResult Edit(Client client)
-    {
-        _db.Entry(client).State = EntityState.Modified;
-        _db.SaveChanges();
-        return RedirectToAction("Index");
-    }
-
-    [HttpPost]
-    public ActionResult Create(Client client)
-    {
-        _db.Clients.Add(client);
-        _db.SaveChanges();
-        return RedirectToAction("Index");
-    }
-
     public ActionResult Details(int id)
     {
-        Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
-        return View(thisClient);
+      Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+      return View(thisClient);
     }
   }
 }
